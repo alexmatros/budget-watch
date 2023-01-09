483114ad63c39549d4c2aa160a63cfce730ffe4a
Anthony3301 <anthonypirvuti@gmail.com>
2023-01-09 09:45:05 -0500

    Added balance, income, and expense tracking

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+  const {transactions} = useContext(GlobalContext);
+  const amountsTransactions = transactions.map(transaction => transaction.amount);
+  const totalAmt = amountsTransactions.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <>
         <h4>Your Balance</h4>
-        <h1>$0.00</h1>
+        <h1>${totalAmt}</h1>
     </>
   )
 }
