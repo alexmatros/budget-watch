a513f38ac930b98caf165f02525cd81593cbf4a2
Anthony3301 <anthonypirvuti@gmail.com>
2023-01-09 17:24:25 -0500

    added bootstrap

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,6 +4,8 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const [isIncome, setIsIncome] = useState(true);
+
     const {addTr} = useContext(GlobalContext);
     const onSubmit = e => {
         e.preventDefault();
@@ -17,6 +19,29 @@ export const AddTransaction = () => {
         addTr(newTr);
     }
 
+    const onAmountChange = e => {
+        setAmount(e.target.value)
+
+        if (amount >= 0) {
+            setIsIncome(true);
+        } else {
+            setIsIncome(false);
+        }
+    }
+
+    const incomeList = (
+        <select>
+            <option value="Salary">Salary</option>
+            <option value="Misc">Misc.</option>
+        </select>
+    )
+
+    const expenseList = (
+        <select>
+            <option value="vegs">vegs</option>
+        </select>
+    )
+
     return (
         <>
             <h3>Add new transaction</h3>
@@ -26,12 +51,10 @@ export const AddTransaction = () => {
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="amount"
-                        >Amount <br />
-                        (negative - expense, positive - income)</label
-                    >
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                    <label htmlFor="amount">Amount</label>
+                    <input type="number" value={amount} onChange={onAmountChange} placeholder="Enter amount..." />
                 </div>
+                {isIncome ? incomeList : expenseList}
                 <button className="btn">Add transaction</button>
             </form>
         </>
