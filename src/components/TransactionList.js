821dea906000000eb9d698e688a6505c1187a28c
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-01-08 23:53:59 -0500

    Began implementation of Context API

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  return (
-    <>
-        <h3>History</h3>
-        <ul className="list">
-            <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-            </li>
-        </ul>
-    </>
-  )
+    const { transactions } = useContext(GlobalContext);
+
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                {transactions.map(transaction => (<li className="minus">
+                    Cash <span>-$400</span><button className="delete-btn">x</button>
+                </li>))}
+            </ul>
+        </>
+    )
 }
