615a03c9f3f71ff0131928e7f2cd80845dc8a8d8
Anthony3301 <anthonypirvuti@gmail.com>
2023-01-09 09:14:54 -0500

    extracted transaction functionality to new file

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
     const { transactions } = useContext(GlobalContext);
@@ -8,9 +9,7 @@ export const TransactionList = () => {
         <>
             <h3>History</h3>
             <ul className="list">
-                {transactions.map(transaction => (<li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>))}
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
             </ul>
         </>
     )
