003c9e52ec92329e97775d3d3eba7b0f35d02779
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-01-09 19:42:30 -0500

    Added pie graph for income and expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,21 +1,60 @@
 import React, {useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const IncomeExpenses = () => {
     const {transactions} = useContext(GlobalContext)
-    const amountsTransactions = transactions.map(transaction => transaction.amount);
-    const income = amountsTransactions.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
-    const expense = amountsTransactions.filter(item => item < 0).reduce((acc, item) => (acc += Math.abs(item)), 0).toFixed(2);
+    const amountsTransactions = transactions.map(transaction => [transaction.amount, transaction.category]);
+
+    console.log(amountsTransactions)
+    const income = amountsTransactions.filter(item => item[0] > 0).reduce((acc, item) => (acc += item[0]), 0).toFixed(2);
+    const expense = amountsTransactions.filter(item => item[0] < 0).reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+
+    const salary = amountsTransactions.filter(item => item[1] === 'Salary').reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const miscInc = amountsTransactions.filter(item => item[1] === "MiscInc").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+
+    const housing = amountsTransactions.filter(item => item[1] === "Housing").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const transportation = amountsTransactions.filter(item => item[1] === "Transportation").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const food = amountsTransactions.filter(item => item[1] === "Food").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const utilities = amountsTransactions.filter(item => item[1] === "Utilities").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const insurance = amountsTransactions.filter(item => item[1] === "Insurance").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
+    const miscExp = amountsTransactions.filter(item => item[1] === "MiscExp").reduce((acc, item) => (acc += Math.abs(item[0])), 0).toFixed(2);
 
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
                 <p className="money plus">${income}</p>
+                <PieChart
+                    rounded
+                    lineWidth={40}
+                    radius={40}
+                    animate={true}
+                    totalValue={income}
+                    data = {[
+                        { title: 'Salary', value: salary, color: '#53C296' },
+                        { title: 'Misc.', value: miscInc, color: '#99D973' },
+                    ]}
+                />
             </div>
             <div>
                 <h4>Expense</h4>
                 <p className="money minus">-${expense}</p>
+                <PieChart
+                    rounded
+                    lineWidth={40}
+                    radius={40}
+                    animate={true}
+                    totalValue={expense}
+                    data = {[
+                        { title: 'Housing', value: housing, color: '#6A2135' },
+                        { title: 'Transportation', value: transportation, color: '#C13C37' },
+                        { title: 'Food', value: food, color: '#DC1C13' },
+                        { title: 'Utilities', value: utilities, color: '#EA4C46' },
+                        { title: 'Insurance', value: insurance, color: '#F07470' },
+                        { title: 'Misc.', value: miscExp, color: '#F1959B' },
+                    ]}
+                />
             </div>
         </div>
     )
